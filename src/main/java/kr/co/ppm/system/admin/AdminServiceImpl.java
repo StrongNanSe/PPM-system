2c394dd8b39bef1c58331e50898522d4686885c1
StrongNanSe <nanseimbird@gmail.com>
2021-11-23 17:40:51 +0900

    AdminServiceImpl.java 생성

---
+++ src/main/java/kr/co/ppm/system/admin/AdminServiceImpl.java
@@ -0,0 +1,20 @@
+package kr.co.ppm.system.admin;
+
+import kr.co.ppm.system.model.Admin;
+
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public boolean login(Admin admin) {
+        return false;
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public boolean viewAdmin(Admin admin) {
+        return false;
+    }
+}
